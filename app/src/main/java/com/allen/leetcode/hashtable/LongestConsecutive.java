8526baa53e8987ed784ef7ed83e46e0eaaf997e5
allen <weijun.ouyang@lerke.cn>
2023-05-12 18:38:45 +0800

    最长连续序列数

---
+++ app/src/main/java/com/allen/leetcode/hashtable/LongestConsecutive.java
@@ -0,0 +1,40 @@
+package com.allen.leetcode.hashtable;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+ * <p>
+ * 请你设计并实现时间复杂度为O(n) 的算法解决此问题。
+ * <p>
+ * 输入：nums = [100,4,200,1,3,2]
+ * 输出：4200
+ * 解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+ * <p>
+ * 输入：nums = [0,3,7,2,5,8,4,6,0,1]
+ * 输出：9
+ * <p>
+ * <p>
+ * HashSet底层使用的HashMap实现的，HashSet中的每个元素都会被保存到 HashMap中的key中，对应的value是一个常量Object对象。
+ * Java中哈希表的数据结构体现：HashMap、HashSet，具体使用则看需要保存键值对，还是键。
+ *
+ * @Author Allen
+ * @Date 05-12-2023 周五 16:25
+ */
+public class LongestConsecutive {
+
+    public static int longestConsecutive(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j <; j++) {
+
+            }
+
+        }
+
+
+        return 0;
+    }
+
+}
--- app/src/test/java/com/allen/leetcode/HashTableUnitTest.java
+++ app/src/test/java/com/allen/leetcode/HashTableUnitTest.java
@@ -1,6 +1,7 @@
 package com.allen.leetcode;
 
 import com.allen.leetcode.hashtable.GroupAnagrams;
+import com.allen.leetcode.hashtable.LongestConsecutive;
 import com.allen.leetcode.hashtable.TwoSum;
 
 import org.junit.Test;
@@ -32,4 +33,11 @@ public class HashTableUnitTest {
         System.out.println("result:" + Arrays.toString(lists.toArray()));
     }
 
+    @Test
+    public void longestConsecutive() {
+        int[] nums = new int[]{100, 4, 200, 1, 3, 2};
+        System.out.println("nums:" + Arrays.toString(nums));
+        int maxLen = LongestConsecutive.longestConsecutive(nums);
+        System.out.println("maxLen:" + maxLen);
+    }
 }
